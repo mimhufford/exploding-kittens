8e08f1f790baf5bc87407fa22ad7b5f1fe28393b
Mim Hufford <mim@hotmail.co.uk>
2017-01-11 22:49:13 +0000

    rough p2p multiplayer web version

--- index.js
+++ index.js
@@ -1,7 +1,125 @@
 const _ = require('lodash')
+const app = require('express')()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const rl = require('readline').createInterface({ input: process.stdin, output: process.stdout })
 
-const setup = numPlayers => {
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+let players = []
+let state
+let active = false
+let turn
+
+io.on('connection', socket => {
+    players.push({ id: socket.id, socket })
+    socket.emit('state', `You are ${socket.id}`)
+    io.emit('state', `${socket.id} entered the game. There are now ${players.length} players.`)
+
+    socket.on('data', data => {
+        if (data.data == 'debug') console.log(state)
+        if (data.data == 'quit') active = false
+        if (data.data == 'start') {
+            active = true
+            state = setup(players)
+            turn = players[0].id
+            io.emit('state', `${turn} to play`)
+            state.players.forEach(p => p.socket.emit('state', "YOUR CARDS: " + p.hand))
+        }
+        if (active) {
+            const current = state.players.filter(p => p.id == data.id)[0]
+            const next = state.players.filter(p => p.player == (current.player + 1) % state.players.length)[0]
+            const isTheirTurn = turn == current.id
+
+            switch (data.data.toUpperCase()) {
+                case 'DONE':
+                    if (isTheirTurn == false) break
+                    current.hand.push(...state.deck.slice(0, current.take))
+                    current.socket.emit('state', "YOUR CARDS: " + current.hand)
+                    state.deck.splice(0, current.take)
+                    current.take = 1
+                    turn = next.id
+                    io.emit('state', `${turn} to play`)
+                    break
+                case 'COUNT':
+                    current.socket.emit('state', state.deck.length)
+                    break
+                case 'FUTURE':
+                    if (isTheirTurn && current.hand.includes('FUTURE')) {
+                        current.hand.splice(current.hand.indexOf('FUTURE'), 1)
+                        current.socket.emit('state', "TOP 3 CARDS: " + state.deck.slice(0, 3))
+                        current.socket.emit('state', "YOUR CARDS: " + current.hand)
+                    }
+                    break
+                case 'SHUFFLE':
+                    if (isTheirTurn && current.hand.includes('SHUFFLE')) {
+                        current.hand.splice(current.hand.indexOf('SHUFFLE'), 1)
+                        state.deck = _.shuffle(state.deck)
+                        io.emit('state', current.id + ' SHUFFLED THE DECK')
+                        current.socket.emit('state', "YOUR CARDS: " + current.hand)
+                    }
+                    break
+                case 'FAVOUR':
+                    if (isTheirTurn && current.hand.includes('FAVOUR')) {
+                        current.hand.splice(current.hand.indexOf('FAVOUR'), 1)
+                        current.hand.push(next.hand.pop())
+                        current.socket.emit('state', "YOUR CARDS: " + current.hand)
+                        next.socket.emit('state', "YOU WERE FAVOURED FROM. YOUR CARDS: " + next.hand)
+                    }
+                    break
+                case 'SKIP':
+                    if (isTheirTurn && current.hand.includes('SKIP')) {
+                        current.hand.splice(current.hand.indexOf('SKIP'), 1)
+                        current.take = Math.max(0, current.take - 1)
+                        current.socket.emit('state', "YOUR CARDS: " + current.hand)
+                    }
+                    break
+                case 'ATTACK':
+                    if (isTheirTurn && current.hand.includes('ATTACK')) {
+                        current.hand.splice(current.hand.indexOf('ATTACK'), 1)
+                        current.take = 0
+                        next.take = 2
+                        current.socket.emit('state', "YOUR CARDS: " + current.hand)
+                        next.socket.emit('state', 'ON YOUR TURN YOU MUST PICK UP 2 CARDS')
+                    }
+                    break
+                case 'PAIR':
+                    if (isTheirTurn) {
+
+                        // search for pairs
+                        const pairs = _(current.hand).countBy().pickBy((v, k) => v > 1).map((v, k) => k).value()
+                        const catPairs = _.intersection(pairs, ['ZOMBIE', 'BIKINI', 'SCHRODINGER', 'MOMMA', 'BLADDER'])
+
+                        if (catPairs.length > 0) {
+                            // remove the 2 cards
+                            current.hand = current.hand.filter((card, index) => index != current.hand.indexOf(catPairs[0]))
+                            current.hand = current.hand.filter((card, index) => index != current.hand.indexOf(catPairs[0]))
+
+                            // steal card from the next player (needs improving obviously)
+                            current.hand.push(next.hand.pop())
+
+                            current.socket.emit('state', "YOUR CARDS: " + current.hand)
+                            next.socket.emit('state', "YOU WERE STOLEN FROM. YOUR CARDS: " + next.hand)
+                        }
+                    }
+                    break
+                default:
+                    break
+            }
+        }
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('state', `${socket.id} left the game. There are now ${players.length} players.`)
+        players = players.filter(p => p.id != socket.id)
+    })
+})
+
+http.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+const setup = players => {
     const cards = {
         NOPE: 5, ATTACK: 4, SKIP: 4, FUTURE: 5, SHUFFLE: 4, FAVOUR: 4,
         ZOMBIE: 4, BIKINI: 4, SCHRODINGER: 4, MOMMA: 4, BLADDER: 4
@@ -12,115 +130,17 @@ const setup = numPlayers => {
         .shuffle()                            // shuffle the deck
         .value()
 
-    const players = Array(numPlayers).fill().map((v, i) => {
+    const gamePlayers = players.map((player, i) => {
         const hand = _(deck).take(4).push('DEFUSE').shuffle().value()
         deck.splice(0, 4)
-        return { player: i, hand, take: 1 }
+        return { id: player.id, socket: player.socket, player: i, hand, take: 1 }
     })
 
     // add correct amount of bombs
-    deck.push(...Array(numPlayers - 1).fill('BOMB'))
+    deck.push(...Array(players.length - 1).fill('BOMB'))
 
     // add extra defuse cards
-    deck.push(...Array(numPlayers == 2 ? 2 : 6 - numPlayers).fill('DEFUSE'))
+    deck.push(...Array(players.length == 2 ? 2 : 6 - players.length).fill('DEFUSE'))
 
-    return [deck, players]
+    return { deck, players: gamePlayers }
 }
-
-const play = (deck, players, turn = 0) => {
-    const current = players[turn]
-    const next = (turn + 1) % players.length
-    console.log(players)
-    rl.question(`Player ${turn}> `, answer => {
-        switch (answer.toUpperCase()) {
-            case 'QUIT':
-                process.exit()
-            case 'SHUFFLE HAND':
-                players[turn].hand = _.shuffle(current.hand)
-                play(deck, players, turn)
-                break
-            case 'SHUFFLE':
-                if (current.hand.includes(answer.toUpperCase())) {
-                    players[turn].hand.splice(current.hand.indexOf(answer.toUpperCase()), 1)
-                    play(_.shuffle(deck), players, turn)
-                }
-                else {
-                    console.log(`You do not have any ${answer} cards`)
-                    play(deck, players, turn)
-                }
-                break
-            case 'PAIR':
-                // search for pairs
-                const pairs = _(current.hand).countBy().pickBy((v, k) => v > 1).map((v, k) => k).value()
-                const catPairs = _.intersection(pairs, ['ZOMBIE', 'BIKINI', 'SCHRODINGER', 'MOMMA', 'BLADDER'])
-
-                if (catPairs.length > 0) {
-                    // remove the 2 cards
-                    players[turn].hand = current.hand.filter((card, index) => index != current.hand.indexOf(catPairs[0]))
-                    players[turn].hand = current.hand.filter((card, index) => index != current.hand.indexOf(catPairs[0]))
-
-                    // steal card from the next player (needs improving obviously)
-                    players[turn].hand.push(players[next].hand.pop())
-                }
-                else
-                    console.log(`You do not have any pairs`)
-
-                play(deck, players, turn)
-                break
-            case 'FAVOUR':
-                if (current.hand.includes(answer.toUpperCase())) {
-                    players[turn].hand = current.hand.filter((card, index) => index != current.hand.indexOf(answer.toUpperCase()))
-                    
-                    // steal card from the next player (needs improving obviously)
-                    players[turn].hand.push(players[next].hand.pop())
-                }
-                else
-                    console.log(`You do not have any ${answer} cards`)
-
-                play(deck, players, turn)
-                break
-            case 'FUTURE':
-                if (current.hand.includes(answer.toUpperCase())) {
-                    players[turn].hand = current.hand.filter((card, index) => index != current.hand.indexOf(answer.toUpperCase()))
-                    console.log(deck.slice(0, 3))
-                }
-                else
-                    console.log(`You do not have any ${answer} cards`)
-                play(deck, players, turn)
-                break
-            case 'ATTACK':
-                if (current.hand.includes(answer.toUpperCase())) {
-                    players[turn].hand = current.hand.filter((card, index) => index != current.hand.indexOf(answer.toUpperCase()))
-                    players[turn].take = 0
-                    players[next].take = 2
-                }
-                else
-                    console.log(`You do not have any ${answer} cards`)
-                play(deck, players, turn)
-                break
-            case 'SKIP':
-                if (current.hand.includes(answer.toUpperCase())) {
-                    players[turn].hand = current.hand.filter((card, index) => index != current.hand.indexOf(answer.toUpperCase()))
-                    players[turn].take = current.take - 1
-                }
-                else
-                    console.log(`You do not have any ${answer} cards`)
-                play(deck, players, turn)
-                break
-            case 'DONE':
-                players[turn].hand.push(...deck.slice(0, current.take))
-                deck.splice(0, current.take)
-                players[turn].take = 1
-                if (current.hand.includes('BOMB'))
-                    play(deck, players, turn)
-                else
-                    play(deck, players, next)
-                break
-            default:
-                console.log("WTF?")
-                play(deck, players, turn)
-        }
-    })
-}
-
-play(...setup(2))
